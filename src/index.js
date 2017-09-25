ef17904bb221406131c48170d8fed09613d2cd04
nox911 <nox.911@gmail.com>
2017-09-26 02:56:55 +0300

    solution with comments

--- src/index.js
+++ src/index.js
@@ -1,46 +1,51 @@
+
 module.exports = function multiply(first, second) {
-  // нужно почистить от излишков кода и комменты
-  var c=[];
-var a=first.split('');
-var b=second.split('');
-var x=[];
-var z=[];
-for (var i=0;i<a.length;i++) {
-	x[i]=parseInt(a[i]);}
-for (var i=0;i<b.length;i++) {
-	z[i]=parseInt(b[i]);
-}
-
-var c=[];
-c.length=x.length*z.length;
-for (var i=0;i<c.length;i++){
-	c[i]=0;
-}
 
 
-for (var i=0; i<=z.length-1;i++) {
-	for (var j=0;j<=x.length-1;j++) {
-		c[x.length*z.length-1-(i+j)]=c[x.length*z.length-1-(i+j)]+x[x.length-1-j]*z[z.length-1-i];
+	var c=[],
+			x=[],
+			z=[];
+	var a=first.split('');
+	var b=second.split('');
+
+
+// string to number for first and second array
+	for (var i=0;i<a.length;i++) {
+		a[i]=+a[i];
+	}	
+	for (var i=0;i<b.length;i++) {
+		b[i]=+b[i];
 	}
-}
 
-for (var i=c.length-1;i>=0;i--) {
-	if (c[i]>9) {
-		c[i-1]=c[i-1]+ Math.floor(c[i]/10);
-		c[i]=c[i]-(Math.floor(c[i]/10)*10);
+// reserv length for result array and define 0 to this array
+	var c=[];
+	c.length=a.length*b.length;
+	for (var i=0;i<c.length;i++){
+		c[i]=0;
 	}
-}
-for(var i=0;c[i]==0;i++) {
+
+// column multiple
+	for (var i=0; i<=b.length-1;i++) {
+		for (var j=0;j<=a.length-1;j++) {
+			c[a.length*b.length-1-(i+j)]=c[a.length*b.length-1-(i+j)]+a[a.length-1-j]*b[b.length-1-i];
+		}
+	}
+// transfer decimal
+	for (var i=c.length-1;i>=0;i--) {
+		if (c[i]>9) {
+			c[i-1]=c[i-1]+ Math.floor(c[i]/10);
+			c[i]=c[i]-(Math.floor(c[i]/10)*10);
+		}
+	}
+// del first none-use zero
+	for(var i=0;c[i]==0;i++) {
 		c.splice(i,1);
 		i--;
-}
-var rez='';
-for(var i=0; i<c.length;i++) {
-rez=rez+c[i];
-}
-
-
-
-return rez;
-
+	}
+// reult from array to one string
+	var rez='';
+	for(var i=0; i<c.length;i++) {
+	rez=rez+c[i];
+	}
+	return rez;
 }
